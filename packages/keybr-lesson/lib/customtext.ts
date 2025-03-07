6d7f8ab896bea0fa726e3d2e136e41d56bf24a7a
Lena Cook <lcook42@gatech.edu>
2025-03-07 14:41:34 -0500

    Update customtext.ts

--- packages/keybr-lesson/lib/customtext.ts
+++ packages/keybr-lesson/lib/customtext.ts
@@ -7,8 +7,6 @@ import { LessonKeys } from "./key.ts";
 import { Lesson } from "./lesson.ts";
 import { lessonProps } from "./settings.ts";
 import { Target } from "./target.ts";
-import { generateFragment } from "./text/fragment.ts";
-import { randomWords, uniqueWords, wordSequence } from "./text/words.ts";
 
 export class CustomTextLesson extends Lesson {
   readonly wordList: readonly string[];
@@ -16,7 +14,7 @@ export class CustomTextLesson extends Lesson {
 
   constructor(settings: Settings, keyboard: Keyboard, model: PhoneticModel) {
     super(settings, keyboard, model);
-    this.wordList = this.#getWordList();
+    this.wordList = []
   }
 
   override get letters() {
@@ -25,41 +23,5 @@ export class CustomTextLesson extends Lesson {
 
   override update(keyStatsMap: KeyStatsMap) {
     return LessonKeys.includeAll(keyStatsMap, new Target(this.settings));
-  }
-
-  override generate(lessonKeys: LessonKeys, rng: RNGStream) {
-    return this.wordList.join(" ");
-  }
-
-  hasFinishedTyping(currentIndex: number): boolean {
-    return currentIndex >= this.wordList.length;
-  }
-
-  #makeWordGenerator(rng: RNGStream) {
-    const randomize = this.settings.get(lessonProps.customText.randomize);
-    if (randomize && this.wordList.length > 0) {
-      return uniqueWords(randomWords(this.wordList, rng));
-    } else {
-      return wordSequence(this.wordList, this);
-    }
-  }
-
-  #getWordList() {
-    const content = this.settings.get(lessonProps.customText.content);
-    const lettersOnly = this.settings.get(lessonProps.customText.lettersOnly);
-    const lowercase = this.settings.get(lessonProps.customText.lowercase);
-    const codePoints = new Set(this.codePoints);
-    if (lettersOnly) {
-      for (const codePoint of codePoints) {
-        if (!this.model.language.includes(codePoint)) {
-          codePoints.delete(codePoint);
-        }
-      }
-    }
-    let text = filterText(content, codePoints);
-    if (lowercase) {
-      text = this.model.language.lowerCase(text);
-    }
-    return text.split(/\s+/);
   }
 }
